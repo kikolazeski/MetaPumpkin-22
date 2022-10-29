53ec84cd4cdb5c53be9c6886fc1aac07239222a8
Sarah Traore <sarah-theresa.traore@informatik.hs-fulda.de>
2022-10-29 16:15:23 +0200

    Frontend Aufbau

---
+++ App-Frontend/hackathon_hs/android/app/src/main/kotlin/com/example/hackathon_hs/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hackathon_hs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ App-Frontend/hackathon_hs/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'SignIn.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hackathon',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text(
+            'Welcome',
+            style: TextStyle(fontSize: 24.0),
+          ),
+          onPressed: () {
+            _navigateToNextScreen(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  void _navigateToNextScreen(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignIn()));
+  }
+}
+
