04f9c577c95dc23cab8cf3d237a3328630af546e
gogamid <gamidliimron@gmail.com>
2022-10-30 04:09:56 +0100

    add frontend submodule

--- App-Backend/backend/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-router.get("/login",loginUser)
-router.post("/signup",signup)
--- App-Frontend/hackathon_hs/android/app/src/main/kotlin/com/example/hackathon_hs/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hackathon_hs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
